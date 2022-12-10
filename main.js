758f33fa901514acb8c52235b613612bad42f767
Adham-Niazy <adhamniazy@gmail.com>
2022-12-10 19:29:29 +0200

    Adjust Coloring the sphere + Roughness of the sphere

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ const geometry = new THREE.SphereGeometry(3, 64, 64);
 // 2) Creating the Material of the Sphere
 const material = new THREE.MeshStandardMaterial({
   color: '#00ffa3',
+  roughness: 0.5
 });
 
 // 3) Combining Geometry + Material = Mesh;
@@ -36,7 +37,8 @@ window.addEventListener('resize', () => {
 
 // Light
 const light = new THREE.PointLight(0xffffff, 1, 100);
-light.position.set(0, 10, 10)
+light.position.set(0, 10, 10);
+light.intensity = 1.25;
 scene.add(light);
 
 // Camera
@@ -72,6 +74,25 @@ const t1 = gsap.timeline({
     duration: 1
   }
 })
-t1.fromTo(mesh.scale, {z:0, x:0, y:0}, {z:1, x:1, y:1});
+t1.fromTo(mesh.scale, { z: 0, x: 0, y: 0 }, { z: 1, x: 1, y: 1 });
 t1.fromTo("nav", { y: "-100%" }, { y: "0" });
 t1.fromTo(".title", { opacity: "0" }, { opacity: "1" });
+
+// Mouse Animation Color.
+let mouseDown = false;
+let rgb = [];
+window.addEventListener('mousedown', () => (mouseDown = true));
+window.addEventListener('mouseup', () => (mouseDown = false));
+
+window.addEventListener('mousemove', (e) => {
+  if (mouseDown) {
+    rgb = [
+      Math.round((e.pageX / sizes.width) * 255),
+      Math.round((e.pageY / sizes.height) * 255),
+      150,
+    ]
+    let newColor = new THREE.Color(`rgb(${rgb.join()})`);
+
+    gsap.to(mesh.material.color, { r: newColor.r, g: newColor.g, b: newColor.b })
+  }
+})
