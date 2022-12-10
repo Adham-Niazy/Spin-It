795c6b2919404ca7b11c37deff6265b5fc623e83
Adham-Niazy <adhamniazy@gmail.com>
2022-12-10 16:29:13 +0200

    Creating the our sphere

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
 import * as THREE from 'three';
 
-//Scene
+// Scene
 const scene = new THREE.Scene();
+
+// Creating the Sphere
+// 1) Creating the Geometry
+const geometry = new THREE.SphereGeometry(3, 64, 64);
+// 2) Creating the Material of the Sphere
+const material = new THREE.MeshStandardMaterial({
+  color: '#00ffa3',
+});
+
+// 3) Combining Geometry + Material = Mesh;
+const mesh = new THREE.Mesh(geometry, material);
+// 4) Then We will add the resulted (MESH) to the Scene
+scene.add(mesh);
