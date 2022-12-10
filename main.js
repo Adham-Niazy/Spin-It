94144fe03c36cb55c643fa99e83f25d3fe26ceb4
Adham-Niazy <adhamniazy@gmail.com>
2022-12-10 16:57:08 +0200

    Adding Lights + Adjusting camera position + rendering the SCENE

--- main.js
+++ main.js
@@ -16,6 +16,18 @@ const mesh = new THREE.Mesh(geometry, material);
 // 4) Then We will add the resulted (MESH) to the Scene
 scene.add(mesh);
 
+// Light
+const light = new THREE.PointLight(0xffffff, 1, 100);
+light.position.set(0, 10, 10)
+scene.add(light);
+
 // Camera
-const camera = new THREE.PerspectiveCamera(45, 800, 600);
+const camera = new THREE.PerspectiveCamera(45, 800 / 600);
+camera.position.z = 20;
 scene.add(camera);
+
+// Renderer
+const canvas = document.querySelector("#webgl");
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(800, 600);
+renderer.render(scene, camera);
